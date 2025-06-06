8da79e4dedc8766ccc542f64549f96a5892b970d
F4DCA <177960329+F4DCA@users.noreply.github.com>
2025-06-06 20:53:25 +0700

    Added RK2Solver
    yippee, idk why is smaller stepsize getting higher error?

--- ODE_Solver.cc
+++ ODE_Solver.cc
@@ -1,5 +1,21 @@
 #include "ODE_Solver.hh"
 
-double ODESolver::solve() {
-    return placeholder;
+std::array<double, 2> ODESolver::rk2solve() {
+    double k1, k2, tMid, yMid;
+    double ti = t0;
+    double yi = y0;
+    int numSteps = static_cast<int>(1.0 / stepSize + 0.5);
+    for (int stepCount = 0;  stepCount < numSteps;  ++stepCount) {
+        k1 = func(ti, yi);
+        tMid = ti + (stepSize/2);
+        yMid = yi + (stepSize/2)*k1;
+        k2 = func(tMid, yMid);
+        ti = ti + stepSize;
+        yi = yi + stepSize*k2;
+    }
+    return {ti, yi};
+}
+
+double ODESolver::func(double t, double y) {
+    return t*t + y;
 }
--- ODE_Solver.hh
+++ ODE_Solver.hh
@@ -1,12 +1,17 @@
 #ifndef H_ODE_SOLVER
 #define H_ODE_SOLVER
+#include <array>
 
 class ODESolver {
     private:
-        double placeholder = 4311;
+        double t0 = 0;
+        double y0 = 1;
+        double stepSize = 0.05;
+        double t, y;
 
     public:
-        double solve();
+        std::array<double, 2> rk2solve();
+        double func(double t, double y);
 };
 
 #endif
